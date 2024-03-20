b2ac30908b8f74374eb763c475aa5bf95b181255
trile <levantri0309@gmail.com>
2024-03-20 21:44:21 +0700

    Add grateful shutdown, docker compose, make file, migrate file, docker image

--- cmd/user_app/main.go
+++ cmd/user_app/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/rs/zerolog/log"
 	"github.com/trile99/go_api_server/internal/app/databases"
 	"github.com/trile99/go_api_server/internal/app/router"
 )
@@ -16,5 +21,20 @@ func main() {
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404) // => 404 "Not Found"
 	})
-	app.Listen(":8080")
+
+	signChan := make(chan os.Signal, 1)
+	// Run Server
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Printf("%v", err.Error())
+		}
+	}()
+	signal.Notify(signChan, os.Interrupt, syscall.SIGTERM)
+	<-signChan
+	log.Print("Stop http server")
+	if err := app.Shutdown(); err != nil {
+		log.Printf("Error while shutdown application %v", err.Error())
+	}
+	close(signChan)
+	log.Printf("Completed")
 }
